d42b249f802b1be05258d27f369ebf1c61fcac29
Joe <joelihai2021@gmail.com>
2021-03-29 11:00:14 +0800

    add file exist

--- products2.py
+++ products2.py
@@ -1,13 +1,22 @@
 
-# 讀取檔案
+import os # operating system
+
 products = []
-with open('products2.csv', 'r', encoding = 'utf-8-sig') as f:
-	for line in f:
-		if '商品,價格' in line:
-			continue
-		name, price = line.strip().split(',')
-		products.append([name, price])
-print(products)
+
+# 檢察檔案在不在
+if os.path.isfile('products2.csv'):
+	# 讀取檔案
+	with open('products2.csv', 'r', encoding = 'utf-8-sig') as f:
+		for line in f:
+			if '商品,價格' in line:
+				continue
+			name, price = line.strip().split(',')
+			products.append([name, price])
+	print(products)
+else:
+	print('找不到檔案')
+
+
 
 # 讓使用者輸入
 while True:
